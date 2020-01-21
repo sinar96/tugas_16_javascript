d0740e3febe34436dd6564f0a9ab95c7fd1b69bd
sinar96 <sinar96>
2020-01-21 21:40:50 +0800

    math min,max

---
+++ tugas16.js
@@ -0,0 +1,15 @@
+function MinMax() {
+    var a = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    var b = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    console.log("Nilai Minimal : ", a);
+    console.log("Nilai Maximal : ", b);
+}
+MinMax();
+
+function MinMax2() {
+    var a = Math.min(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+    var b = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+    console.log("Nilai Minimal : ", a);
+    console.log("Nilai Maximal : ", b);
+}
+MinMax2();
